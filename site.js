76dc2326156e1ff59cda7f7a46818d416e8887b1
Karl Stolley <karl.stolley@gmail.com>
2020-08-31 16:01:16 -0400

    Demonstrate DOMContentLoaded vs free-standing func call

---
+++ site.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+  console.log('The DOM is ready for action');
+})
+
+console.log("Odds are, the DOM is loaded because I'm at the very end")
