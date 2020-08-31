9bb09c7927261819635a5bc59f78540876405b03
Karl Stolley <karl.stolley@gmail.com>
2020-08-31 16:03:14 -0400

    Demonstrate query selector and click event

--- site.js
+++ site.js
@@ -2,6 +2,12 @@ document.addEventListener('DOMContentLoaded', function(){
   console.log('The DOM is ready for action');
 })
 
+var h1 = document.querySelector('h1');
+
+h1.addEventListener('click', function() {
+  h1.innerText = "Someone clicked me!"
+});
+
 console.log("Odds are, the DOM is loaded because I'm at the very end")
 
 console.log("One");
