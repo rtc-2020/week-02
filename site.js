d100dc86e7abfc04d6ed4fe3db1ba9136ff92de3
Karl Stolley <karl.stolley@gmail.com>
2020-08-31 16:02:14 -0400

    Demonstrate async with a setTimeout() call

--- site.js
+++ site.js
@@ -3,3 +3,9 @@ document.addEventListener('DOMContentLoaded', function(){
 })
 
 console.log("Odds are, the DOM is loaded because I'm at the very end")
+
+console.log("One");
+setTimeout(function() {
+    console.log("Two")
+  }, 5000);
+console.log("Buckle my shoe");
